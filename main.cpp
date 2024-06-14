847a491ddaa00ad033477e33df38223398b3982f
OmmrGAZAR315 <omarahmedelgazzar600@gmail.com>
2024-06-15 00:03:23 +0300

    comparing with and wihtout DP

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 
 std::map<int, long long> dic;
 
-long long fibonacci(int n) {
+#include <iostream>
+
+int fibonacci(int n) {
+    if (n == 1)
+        return 0;
+    else if (n == 2)
+        return 1;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+long long fibonacci_with_DP(int n) {
     if (n == 1)
         return 0;
     else if (n == 2)
@@ -11,13 +22,14 @@ long long fibonacci(int n) {
     else if (dic.find(n) != dic.end()) {
         return dic[n];
     } else {
-        long long result = fibonacci(n - 1) + fibonacci(n - 2);
+        long long result = fibonacci_with_DP(n - 1) + fibonacci_with_DP(n - 2);
         dic[n] = result;
         return result;
     }
 }
 
 int main() {
-    std::cout << fibonacci(99) << std::endl;
+//    std::cout << fibonacci(99) << std::endl;
+    std::cout << fibonacci_with_DP(99) << std::endl;
     return 0;
 }
